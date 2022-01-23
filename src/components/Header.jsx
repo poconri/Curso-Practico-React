cf48292893e472dfb059827290d08e8d76fbfee6
poconri <pocon.ri@gmail.com>
2022-01-22 22:50:19 -0600

    adding icons to webpack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import '../styles/Header.scss';
+import '@styles/Header.scss';
+
+import Menu from '@icons/icon_menu.svg';
+import  Logo from '@logos/logo_yard_sale.svg';
+import  ShoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
     return(
         <nav>
-            <img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+            <img src={ Menu } alt="menu" className="menu" />
             <div className="navbar-left">
-                <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+                <img src= { Logo } alt="logo" className="nav-logo" />
                 <ul>
                     <li>
                         <a href="/">All</a>
@@ -32,7 +36,7 @@ const Header = () => {
                 <ul>
                     <li className="navbar-email">platzi@example.com</li>
                     <li className="navbar-shopping-cart">
-                    <img src="./icons/icon_shopping_cart.svg" alt="shopping cart"/>
+                    <img src= { ShoppingCart } alt="shopping cart"/>
                     <div>2</div>
                     </li>
                 </ul>
