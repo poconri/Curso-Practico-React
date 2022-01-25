2862a53c75557f94edd34839f700e7f511c26d02
poconri <pocon.ri@gmail.com>
2022-01-25 02:15:40 -0600

    adding state to the Header's count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import '@styles/Header.scss';
 import Menu from '@components/Menu';
 import menu from '@icons/icon_menu.svg';
 import  Logo from '@logos/logo_yard_sale.svg';
+import AppContext from '@context/AppContext';
 import  ShoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
     const [toggle, setToggle ] = useState(false);
+    const { state } = useContext(AppContext);
 
     const handleToggle = () => {
         setToggle(!toggle);
@@ -45,7 +47,7 @@ const Header = () => {
                     </li>
                     <li className="navbar-shopping-cart">
                     <img src= { ShoppingCart } alt="shopping cart"/>
-                    <div>2</div>
+                    <div>{state.cart.length > 0 ? state.cart.length : null }</div>
                     </li>
                 </ul>
             </div>
