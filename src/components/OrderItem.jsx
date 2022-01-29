79fd70b361a36a48a0b37c04dac220f84cc02485
poconri <pocon.ri@gmail.com>
2022-01-29 01:26:24 -0600

    adding the last logic

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '@styles/OrderItem.scss';
-
-import close from '@icons/icon_close.png';
+import close from '@icons/icon_close.png'
 
 const OrderItem = ({ product }) => {
-    return(
-        <div className="OrderItem">
-            <figure>
-				<img src={ product.images[0] } alt= { product.title } />
+	const { removeFromCart } = useContext(AppContext);
+
+	const handleRemove = product => {
+		removeFromCart(product);
+	}
+
+	return (
+		<div className="OrderItem">
+			<figure>
+				<img src={product.images[0]} alt={product.title} />
 			</figure>
-			<p>{ product.title }</p>
-			<p>Q{ product.price }</p>
-			<img src= { close } alt="close" />
-        </div>
-    );
+			<p>{product.title}</p>
+			<p>${product.price}</p>
+			<img src={close} alt="close" onClick={() => handleRemove(product)} />
+		</div>
+	);
 }
 
 export default OrderItem;
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 import OrderItem from '@components/OrderItem';
-import AppContext from '@context/AppContext';
-import "@styles/MyOrder.scss";
+import AppContext from '../context/AppContext';
+import '@styles/MyOrder.scss';
 import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
@@ -12,28 +12,29 @@ const MyOrder = () => {
 		const sum = state.cart.reduce(reducer, 0);
 		return sum;
 	}
-    return(
-        <aside className="MyOrder">
+
+	return (
+		<aside className="MyOrder">
 			<div className="title-container">
-				<img src= { arrow } alt="arrow" />
+				<img src={arrow} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				{ state.cart.map(product => (
-					<OrderItem product = { product } key= {`orderItem-${product.id}`} />
-				)) }
+				{state.cart.map(product => (
+					<OrderItem product={product} key={`orderItem-${product.id}`} />
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>Q{ sumTotal() }</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
 				</button>
 			</div>
 		</aside>
-    );
+	);
 }
 
 export default MyOrder;
